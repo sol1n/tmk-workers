7ef9339575677a6db09d24cf7da0d9c2b8dd84f2
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-09-30 21:13:57 +0700

    Team favorites two fields support & scores element creation

--- app/Workers/TeamFavoritesWorker.php
+++ app/Workers/TeamFavoritesWorker.php
@@ -8,10 +8,20 @@ use Appercode\User;
 use Appercode\Backend;
 use Appercode\Element;
 
+use Carbon\Carbon;
+
 use Illuminate\Support\Collection;
 
 class TeamFavoritesWorker extends BaseWorker
 {
+    const DATES = [
+        '1.10.2018',
+        '2.10.2018',
+        '3.10.2018',
+        '4.10.2018',
+        '5.10.2018'
+    ];
+
     protected function getTeams(): Collection
     {
         return Element::list('TeamStandingsTeams', $this->user->backend, [
@@ -78,14 +88,39 @@ class TeamFavoritesWorker extends BaseWorker
         }
     }
 
+    protected function createEventsScores(array $eventsIds, Collection $teams)
+    {
+        foreach ($eventsIds as $eventId) {
+            foreach ($teams as $team) {
+                $existed = Element::list('TeamStandingsScores', $this->user->backend, [
+                    'take' => -1,
+                    'where' => [
+                        'eventId' => $eventId,
+                        'teamId' => $team->id
+                    ]
+                ]);
+
+                if ($existed->count() == 0) {
+                    foreach (self::DATES as $date) {
+                        Element::create('TeamStandingsScores', [
+                            'teamId' => $team->id,
+                            'eventId' => $eventId,
+                            'date' => Carbon::parse($date, 'UTC')->toAtomString(),
+                        ], $this->user->backend);
+                    }
+                }
+            }
+        }
+    }
+
     public function handle()
     {
         $teams = $this->getTeams();
 
         $eventsIds = [];
         $teams->each(function ($item) use (&$eventsIds) {
-            if (isset($item->fields['eventsIds']) && is_array($item->fields['eventsIds'])) {
-                foreach ($item->fields['eventsIds'] as $eventId) {
+            if (isset($item->fields['eventsIds1']) && is_array($item->fields['eventsIds1'])) {
+                foreach ($item->fields['eventsIds1'] as $eventId) {
                     $eventsIds[$eventId] = true;
                 }
             }
@@ -94,13 +129,40 @@ class TeamFavoritesWorker extends BaseWorker
         $this->markEvents(array_keys($eventsIds));
 
         $teams->each(function ($team) {
-            if (!isset($team->fields['eventsIds']) or !is_array($team->fields['eventsIds']) or count($team->fields['eventsIds']) == 0) {
+            if (!isset($team->fields['eventsIds1']) or !is_array($team->fields['eventsIds1']) or count($team->fields['eventsIds1']) == 0) {
                 return null;
             }
 
-            if (isset($team->fields['userIds']) && is_array($team->fields['userIds'])) {
-                foreach ($team->fields['userIds'] as $userId) {
-                    $this->createFavoriteItem($team->fields['eventsIds'], $userId);
+            if (isset($team->fields['userIds1']) && is_array($team->fields['userIds1'])) {
+                foreach ($team->fields['userIds1'] as $userId) {
+                    $this->createFavoriteItem($team->fields['eventsIds1'], $userId);
+                }
+            }
+        });
+
+        $allEvents = $eventsIds;
+        $eventsIds = [];
+        $teams->each(function ($item) use (&$eventsIds) {
+            if (isset($item->fields['eventsIds2']) && is_array($item->fields['eventsIds2'])) {
+                foreach ($item->fields['eventsIds2'] as $eventId) {
+                    $eventsIds[$eventId] = true;
+                }
+            }
+        });
+
+        $this->markEvents(array_keys($eventsIds));
+        $allEvents = array_merge($allEvents, $eventsIds);
+
+        $this->createEventsScores(array_keys($allEvents), $teams);
+
+        $teams->each(function ($team) {
+            if (!isset($team->fields['eventsIds2']) or !is_array($team->fields['eventsIds2']) or count($team->fields['eventsIds2']) == 0) {
+                return null;
+            }
+
+            if (isset($team->fields['userIds2']) && is_array($team->fields['userIds2'])) {
+                foreach ($team->fields['userIds2'] as $userId) {
+                    $this->createFavoriteItem($team->fields['eventsIds2'], $userId);
                 }
             }
         });
